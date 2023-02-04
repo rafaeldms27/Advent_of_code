bde52a4ac6320e8837d7f8d8897ebc781ba99d9a
rafaeldms27 <86275960+rafaeldms27@users.noreply.github.com>
2023-02-04 17:25:40 -0600

    Update day1_most_calorie_elf.py

--- day1_most_calorie_elf.py
+++ day1_most_calorie_elf.py
@@ -1,10 +1,5 @@
-import numpy as np
-#open the file
 f = open('input1.txt','r')
 lista = f.read().rsplit('\n')
-#print(lista[10])
-#print(lista[11])
-#print(lista[12])
 total_cal_list = []
 total_cal_sum = 0
 for x in lista:
@@ -13,4 +8,5 @@ for x in lista:
 	else:
 		total_cal_list.append(total_cal_sum)
 		total_cal_sum = 0
+f.close()
 print(max(total_cal_list))
